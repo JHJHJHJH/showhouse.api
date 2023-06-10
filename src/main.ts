6c2218fd7d57e8b6389dac8f6d6a7246951890c8
Han <JunHan.Loh@lendlease.com>
2023-06-10 09:52:16 +0800

    x

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(pipe);
   app.enableCors({
-    origin: ['*'],
+    origin: [process.env.SHOWHOUSE_URL_PROD, process.env.SHOWHOUSE_URL_DEV],
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: [
       'Content-Type',
