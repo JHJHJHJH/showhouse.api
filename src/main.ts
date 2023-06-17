b5a2fb451e16f4d14119af998752642a706c9374
Han <JunHan.Loh@lendlease.com>
2023-06-17 11:27:25 +0800

    OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -11,26 +11,24 @@ async function bootstrap() {
   // console.log(`Port: ${process.env.POSTGRES_PORT} `);
   // console.log(`URA API KEY: ${process.env.URA_API_KEY} `);
   // console.log(`PG DB: ${process.env.POSTGRES_DB} `);
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('v1/api');
   const port = process.env.PORT || 8080;
   const pipe = new ValidationPipe({ whitelist: true });
-  app.setGlobalPrefix('api/');
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(pipe);
   app.useGlobalFilters(new SupertokensExceptionFilter());
-  // app.enableCors({
-  //   origin: '*',
-  //   preflightContinue: true,
-  //   methods: 'GET, PUT, POST, DELETE',
-  //   allowedHeaders: [
-  //     'Content-Type',
-  //     'Authorization',
-  //     ...supertokens.getAllCORSHeaders(),
-  //   ],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: '*',
+    preflightContinue: true,
+    methods: 'GET, PUT, POST, DELETE, OPTIONS',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      ...supertokens.getAllCORSHeaders(),
+    ],
+    credentials: true,
+  });
   console.log(`App running on <${process.env.NODE_ENV}>....`);
   console.log(`Listening to PORT ${port}....`);
   await app.listen(port);
