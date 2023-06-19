c624c0cd276c1cd94096f5f3efcb035a937d9c3a
Han <JunHan.Loh@lendlease.com>
2023-06-19 23:16:03 +0800

    regex

--- src/main.ts
+++ src/main.ts
@@ -27,15 +27,16 @@ async function bootstrap() {
     'https://showhouse.app/',
   ];
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    // origin: function (origin, callback) {
+    //   if (whitelist.indexOf(origin) !== -1) {
+    //     console.log('allowed cors for:', origin);
+    //     callback(null, true);
+    //   } else {
+    //     console.log('blocked cors for:', origin);
+    //     callback(new Error('Not allowed by CORS'));
+    //   }
+    // },
+    origin: /showhouse\.app$/,
     preflightContinue: false,
     methods: 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
     allowedHeaders: [
