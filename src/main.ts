85e9511ab86f0bff925f3a46e02e4fedb3e103c6
Han <JunHan.Loh@lendlease.com>
2023-06-20 00:00:50 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -27,15 +27,7 @@ async function bootstrap() {
     'https://showhouse.app',
   ];
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: whitelist,
     preflightContinue: false,
     methods: 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
     allowedHeaders: [
